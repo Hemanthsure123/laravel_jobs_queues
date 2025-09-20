a6ad9ec0104939726832073cba9f2180e96d9148
Hemanth123 <hemanthsure3@gmail.com>
2025-09-20 14:33:20 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
-use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QueueController;
+
+Route::get('/queue-form', [QueueController::class, 'showForm']);
+Route::post('/process-queue', [QueueController::class, 'processForm']);
+
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register.form');
 Route::post('/register', [RegisterController::class, 'register'])->name('register');
-// Optional: Dashboard route
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
